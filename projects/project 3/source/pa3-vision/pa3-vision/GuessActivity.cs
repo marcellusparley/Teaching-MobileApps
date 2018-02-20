d78918014c2d54c06559d764b9ddcdfc3845b219
marcellus parley <msp261@humboldt.edu>
2018-02-19 16:48:53 -0800

    Today's work. Only just getting started.

---
+++ projects/project 3/source/pa3-vision/pa3-vision/GuessActivity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace pa3_vision
+{
+    [Activity(Label = "GuessActivity")]
+    public class GuessActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Guess);
+
+            // Display in ImageView. We will resize the bitmap to fit the display.
+            // Loading the full sized image will consume too much memory
+            // and cause the application to crash.
+            ImageView imageView = FindViewById<ImageView>(Resource.Id.takenPictureImageView);
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = imageView.Height;
+
+            //AC: workaround for not passing actual files
+            Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)data.Extras.Get("data");
+
+            //convert bitmap into stream to be sent to Google API
+            string bitmapString = "";
+            using (var stream = new System.IO.MemoryStream())
+            {
+                bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 0, stream);
+
+                var bytes = stream.ToArray();
+                bitmapString = System.Convert.ToBase64String(bytes);
+            }
+
+            //credential is stored in "assets" folder
+            string credPath = "google_api.json";
+            Google.Apis.Auth.OAuth2.GoogleCredential cred;
+
+            //Load credentials into object form
+            using (var stream = Assets.Open(credPath))
+            {
+                cred = Google.Apis.Auth.OAuth2.GoogleCredential.FromStream(stream);
+            }
+            cred = cred.CreateScoped(Google.Apis.Vision.v1.VisionService.Scope.CloudPlatform);
+
+            // By default, the library client will authenticate 
+            // using the service account file (created in the Google Developers 
+            // Console) specified by the GOOGLE_APPLICATION_CREDENTIALS 
+            // environment variable. We are specifying our own credentials via json file.
+            var client = new Google.Apis.Vision.v1.VisionService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                ApplicationName = "cs480-mobileapps-pa3-msp261",
+                HttpClientInitializer = cred
+            });
+
+            //set up request
+            var request = new Google.Apis.Vision.v1.Data.AnnotateImageRequest();
+            request.Image = new Google.Apis.Vision.v1.Data.Image();
+            request.Image.Content = bitmapString;
+
+            //tell google that we want to perform label detection
+            request.Features = new List<Google.Apis.Vision.v1.Data.Feature>();
+            request.Features.Add(new Google.Apis.Vision.v1.Data.Feature() { Type = "LABEL_DETECTION" });
+            var batch = new Google.Apis.Vision.v1.Data.BatchAnnotateImagesRequest();
+            batch.Requests = new List<Google.Apis.Vision.v1.Data.AnnotateImageRequest>();
+            batch.Requests.Add(request);
+
+            //send request.  Note that I'm calling execute() here, but you might want to use
+            //ExecuteAsync instead
+            var apiResult = client.Images.Annotate(batch).Execute();
+
+            if (bitmap != null)
+            {
+                imageView.SetImageBitmap(bitmap);
+                imageView.Visibility = Android.Views.ViewStates.Visible;
+                bitmap = null;
+            }
+
+            // Dispose of the Java side bitmap.
+            System.GC.Collect();
+        }
+    }
+}
